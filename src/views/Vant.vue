661cdc6aa487aba8d6ffb3775a9745e14a486b72
lihua <lihua@bestvike.com>
2019-01-03 15:09:52 +0800

    初始化

---
+++ src/views/Vant.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+  </div>
+</template>
+
+<script>
+    import { Button } from 'vant';
+
+    export default {
+        components: {
+            [Button.name]: Button
+        }
+    };
+</script>
